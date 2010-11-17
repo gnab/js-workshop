0e94c9600edc37c521d384f733e00279b1e9914a
Thomas Adamcik <adamcik@samfundet.no>
2010-11-17 09:59:31 +0100

    Setup jslint config

--- public/script/editor.js
+++ public/script/editor.js
@@ -279,8 +279,10 @@ function updateLayout() {
 
 function checkForLintErrors(code) {
   lintconsole.clear()
+  var options = '/*jslint devel: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */';
+  var global = '/*global assert: true, iter: true */';
 
-  if (!JSLINT(code)) {
+  if (!JSLINT(options + global + code)) {
     for (var i = 0; i < JSLINT.errors.length; i++) {
       var lint = JSLINT.errors[i];
         lintconsole.error(lint.id + ' ' + lint.reason, lint.line);
